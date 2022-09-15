0dfc3fb599860158cc156788d2c351cb1a25e270
Simon Mießler <simon.miessler@gmx.net>
2022-09-15 22:46:21 +0200

    Added support for .NET 4.7.1 and 4.8

--- AssemblyInfo/Applications/PatchInternalsVisibleTo.cs
+++ AssemblyInfo/Applications/PatchInternalsVisibleTo.cs
@@ -177,7 +177,9 @@ namespace AssemblyInfo.Applications
 		{
 			var paths = new[]
 			{
-				@"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\x64\"
+				@"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\x64\",
+				@"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.1 Tools\x64\",
+				@"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\"
 			};
 
 			foreach (var path in paths)
